4bfc399e1dd79afa24d888871789136f6907234b
Ibrahim Khan <aitvetibrahim@gmail.com>
2022-08-17 11:39:48 +0600

    title change

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -17,7 +17,7 @@ class _HomePageState extends State<HomePage> {
     size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
-        title: Text('ListView Example'),
+        title: Text('Favorite Hotel'),
       ),
       body: ListView.builder(
         itemCount: hotels.length,
