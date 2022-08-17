9b4f4c0130df7f2a62fc1c0d6fa40757c12f7439
Ibrahim Khan <aitvetibrahim@gmail.com>
2022-08-17 12:03:38 +0600

    all text and image set all field

--- lib/hotel_item.dart
+++ lib/hotel_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_listview/temp_db.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 class HotelItem extends StatefulWidget {
   final Hotel hotel;
@@ -12,37 +13,70 @@ class HotelItem extends StatefulWidget {
 class _HotelItemState extends State<HotelItem> {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16)
-      ),
-      child: Column(
-        children: [
-          Stack(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                //borderRadius: BorderRadius.only(topRight: Radius.circular(16),topLeft: Radius.circular(16)),
-                child: Image.network(
-                  widget.hotel.image,
-                  width: double.maxFinite,
-                  height: 200,
-                  fit: BoxFit.cover,
+    return Padding(
+      padding: const EdgeInsets.all(6.0),
+      child: Card(
+        elevation: 10,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16)
+        ),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8),
+                  //borderRadius: BorderRadius.only(topRight: Radius.circular(16),topLeft: Radius.circular(16)),
+                  child: Image.network(
+                    widget.hotel.image,
+                    width: double.maxFinite,
+                    height: 200,
+                    fit: BoxFit.cover,
+                  ),
                 ),
+                Positioned(
+                  right: 0,
+                  child: IconButton(
+                    icon: Icon(widget.hotel.favorite ? Icons.favorite : Icons.favorite_border),
+                    onPressed: (){},
+                  ),
+                )
+              ],
+            ),
+            ListTile(
+              title: Text(widget.hotel.name,style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+              trailing: Text('\$${widget.hotel.price}',style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(widget.hotel.address),
+                  Row(
+                    children: [
+                      RatingBar.builder(
+                          itemSize: 20,
+                          ignoreGestures: true,
+                          initialRating: 3,
+                          minRating: 1,
+                          direction: Axis.horizontal,
+                          allowHalfRating: true,
+                          itemCount: 5,
+                          itemPadding: EdgeInsets.symmetric(horizontal: 0),
+                          itemBuilder: (context, value){
+                            return Icon(Icons.star, color: Colors.amber,);
+                          },
+                          onRatingUpdate: (value){
+
+                          }
+                      ),
+                      SizedBox(width: 10,),
+                      Text('80 reviews'),
+                    ],
+                  )
+                ],
               ),
-              Positioned(
-                right: 0,
-                child: IconButton(
-                  icon: Icon(widget.hotel.favorite ? Icons.favorite : Icons.favorite_border),
-                  onPressed: (){},
-                ),
-              )
-            ],
-          ),
-          ListTile(
-            title: Text(widget.hotel.name),
-          )
-        ],
+            ),
+          ],
+        ),
       ),
     );
   }
