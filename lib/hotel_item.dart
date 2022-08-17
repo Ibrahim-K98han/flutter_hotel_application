7fcfa64f7e31d4bebef82ca2803d64f2cf25c8eb
Ibrahim Khan <aitvetibrahim@gmail.com>
2022-08-17 12:23:33 +0600

    added animation

--- lib/hotel_item.dart
+++ lib/hotel_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_listview/pages/second_page.dart';
 import 'package:flutter_listview/temp_db.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
@@ -15,67 +16,75 @@ class _HotelItemState extends State<HotelItem> {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(6.0),
-      child: Card(
-        elevation: 10,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(16)
-        ),
-        child: Column(
-          children: [
-            Stack(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(8),
-                  //borderRadius: BorderRadius.only(topRight: Radius.circular(16),topLeft: Radius.circular(16)),
-                  child: Image.network(
-                    widget.hotel.image,
-                    width: double.maxFinite,
-                    height: 200,
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                Positioned(
-                  right: 0,
-                  child: IconButton(
-                    icon: Icon(widget.hotel.favorite ? Icons.favorite : Icons.favorite_border),
-                    onPressed: (){},
-                  ),
-                )
-              ],
-            ),
-            ListTile(
-              title: Text(widget.hotel.name,style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
-              trailing: Text('\$${widget.hotel.price}',style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+      child: InkWell(
+        onTap: (){
+          Navigator.pushNamed(context, SecondPage.routeName,arguments: widget.hotel);
+        },
+        child: Card(
+          elevation: 10,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(16)
+          ),
+          child: Column(
+            children: [
+              Stack(
                 children: [
-                  Text(widget.hotel.address),
-                  Row(
-                    children: [
-                      RatingBar.builder(
-                          itemSize: 20,
-                          ignoreGestures: true,
-                          initialRating: 3,
-                          minRating: 1,
-                          direction: Axis.horizontal,
-                          allowHalfRating: true,
-                          itemCount: 5,
-                          itemPadding: EdgeInsets.symmetric(horizontal: 0),
-                          itemBuilder: (context, value){
-                            return Icon(Icons.star, color: Colors.amber,);
-                          },
-                          onRatingUpdate: (value){
-
-                          }
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    //borderRadius: BorderRadius.only(topRight: Radius.circular(16),topLeft: Radius.circular(16)),
+                    child: Hero(
+                      tag: widget.hotel.id,
+                      child: Image.network(
+                        widget.hotel.image,
+                        width: double.maxFinite,
+                        height: 200,
+                        fit: BoxFit.cover,
                       ),
-                      SizedBox(width: 10,),
-                      Text('80 reviews'),
-                    ],
+                    ),
+                  ),
+                  Positioned(
+                    right: 0,
+                    child: IconButton(
+                      icon: Icon(widget.hotel.favorite ? Icons.favorite : Icons.favorite_border),
+                      onPressed: (){},
+                    ),
                   )
                 ],
               ),
-            ),
-          ],
+              ListTile(
+                title: Text(widget.hotel.name,style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),),
+                trailing: Text('\$${widget.hotel.price}',style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),),
+                subtitle: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(widget.hotel.address),
+                    Row(
+                      children: [
+                        RatingBar.builder(
+                            itemSize: 20,
+                            ignoreGestures: true,
+                            initialRating: 3,
+                            minRating: 1,
+                            direction: Axis.horizontal,
+                            allowHalfRating: true,
+                            itemCount: 5,
+                            itemPadding: EdgeInsets.symmetric(horizontal: 0),
+                            itemBuilder: (context, value){
+                              return Icon(Icons.star, color: Colors.amber,);
+                            },
+                            onRatingUpdate: (value){
+
+                            }
+                        ),
+                        SizedBox(width: 10,),
+                        Text('80 reviews'),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/pages/second_page.dart
+++ lib/pages/second_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_listview/temp_db.dart';
 
 class SecondPage extends StatefulWidget {
   static const String routeName = '/second_page';
@@ -9,13 +10,43 @@ class SecondPage extends StatefulWidget {
 }
 
 class _SecondPageState extends State<SecondPage> {
+  late Hotel hotel;
+
+  @override
+  void didChangeDependencies() {
+    hotel = ModalRoute.of(context)!.settings.arguments as Hotel;
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Second Page'),
+        title: Text(hotel.name),
       ),
-      body: Center(),
+      body: ListView(
+        children: [
+          Hero(
+            tag: hotel.id,
+            child: Image.network(
+              hotel.image,
+              width: double.maxFinite,
+              height: 350,
+              fit: BoxFit.cover,
+            ),
+          ),
+          ListTile(
+            title: Text(hotel.name),
+            subtitle: Text(hotel.address),
+            trailing: Text('\$${hotel.price}'),
+          ),
+          SizedBox(height: 20,),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(hotel.description),
+          )
+        ],
+      ),
     );
   }
 }
