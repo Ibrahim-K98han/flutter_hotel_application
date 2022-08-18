b86cb448d25fe1a64a49fdc6921fce7282e08b65
Ibrahim Khan <aitvetibrahim@gmail.com>
2022-08-18 09:43:37 +0600

    photofilter page added complete

--- lib/pages/photo_filter_page.dart
+++ lib/pages/photo_filter_page.dart
@@ -9,10 +9,48 @@ class PhotoFilterPage extends StatefulWidget {
 }
 
 class _PhotoFilterPageState extends State<PhotoFilterPage> {
+  late Size size;
+  bool isExpanded = false;
   @override
   Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size;
     return Scaffold(
-
+      body: Container(
+        width: size.width,
+        height: size.height,
+        child: Stack(
+          children: [
+            Positioned.fill(child: Image.asset('images/radison_blu_dhaka.jpg',fit: BoxFit.cover,),
+            ),
+            AnimatedPositioned(
+              curve: Curves.easeInOut,
+              duration: Duration(microseconds: 1000),
+              bottom: isExpanded ? 0 : -(size.height / 2) + 70,
+              child: Container(
+                color: Colors.black54,
+                width: size.width,
+                height: size.height / 2,
+                child: ListView(
+                  padding: EdgeInsets.all(0.0),
+                  children: [
+                    ListTile(
+                      title: Text('Filter Options',style: TextStyle(fontSize: 18,color: Colors.white),),
+                      trailing: IconButton(
+                        onPressed: (){
+                          setState((){
+                            isExpanded = !isExpanded;
+                          });
+                        },
+                        icon: Icon(isExpanded ? Icons.keyboard_arrow_down : Icons.keyboard_arrow_up, color: Colors.white,),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
