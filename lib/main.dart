1bacadb679161705e8ad230076711cb05fbd0394
Ibrahim Khan <aitvetibrahim@gmail.com>
2022-08-18 09:19:37 +0600

    photofilter page added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_listview/pages/home_page.dart';
+import 'package:flutter_listview/pages/photo_filter_page.dart';
 import 'package:flutter_listview/pages/second_page.dart';
 
 void main() {
@@ -17,10 +18,11 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: HomePage.routeName,
+      initialRoute: PhotoFilterPage.routeName,
       routes: {
         HomePage.routeName : (context) => HomePage(),
         SecondPage.routeName : (context) => SecondPage(),
+        PhotoFilterPage.routeName : (context) => PhotoFilterPage(),
       },
     );
   }
---
+++ lib/pages/photo_filter_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PhotoFilterPage extends StatefulWidget {
+  static const String routeName = '/';
+  const PhotoFilterPage({Key? key}) : super(key: key);
+
+  @override
+  State<PhotoFilterPage> createState() => _PhotoFilterPageState();
+}
+
+class _PhotoFilterPageState extends State<PhotoFilterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
